162da4e8ed72a45e34280bedc0a594c4d06ebd2c
Ivan Pizarroso <ivnp31@gmail.com>
2023-01-04 18:59:15 +0100

    finish web 1.0.0

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  switch: boolean = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  changeSwitch(): void {
+    this.switch = !this.switch;
   }
-
 }
